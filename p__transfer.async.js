96d5954ffdfbd7cbac13da6d8a75d34a444d1dc6
avaxmarket <151865899+avaxmarket@users.noreply.github.com>
2023-11-24 05:07:03 +0800

    Add files via upload

--- p__transfer.async.js
+++ p__transfer.async.js
@@ -1,12 +1,12 @@
 "use strict";(self.webpackChunk=self.webpackChunk||[]).push([[395],{9987:function(u,r,t){t.r(r);var o=t(2086),f=function(){return(0,o.jsx)("div",{children:(0,o.jsx)("pre",{children:`
 
-      ----mint----
-      
-      data:,{"p":"asc-20","op":"mint","tick":"aval","amt":"100000000"}
+              ----mint----
+              
+              data:,{"p":"asc-20","op":"mint","tick":"aval","amt":"100000000"}
 
-      Encoded and sent to the recipient, and does not support contracts
+              Encoded and sent to the recipient, and does not support contracts
 
-      ----transfer----
+              ----transfer----
 
-      data:,{"p":"asc-20","op":"transfer","tick":"aval","to":[{"recv":"0x000","amt":"10"}]}
-`})})};r.default=f},1836:function(u,r,t){var o=t(3236),f=Symbol.for("react.element"),c=Symbol.for("react.fragment"),p=Object.prototype.hasOwnProperty,m=o.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,k={key:!0,ref:!0,__self:!0,__source:!0};function d(a,e,i){var n,s={},_=null,l=null;i!==void 0&&(_=""+i),e.key!==void 0&&(_=""+e.key),e.ref!==void 0&&(l=e.ref);for(n in e)p.call(e,n)&&!k.hasOwnProperty(n)&&(s[n]=e[n]);if(a&&a.defaultProps)for(n in e=a.defaultProps,e)s[n]===void 0&&(s[n]=e[n]);return{$$typeof:f,type:a,key:_,ref:l,props:s,_owner:m.current}}r.Fragment=c,r.jsx=d,r.jsxs=d},2086:function(u,r,t){u.exports=t(1836)}}]);
+              data:,{"p":"asc-20","op":"transfer","tick":"aval","to":[{"recv":"0x000","amt":"2"}]}
+        `})})};r.default=f},1836:function(u,r,t){var o=t(3236),f=Symbol.for("react.element"),c=Symbol.for("react.fragment"),p=Object.prototype.hasOwnProperty,m=o.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,k={key:!0,ref:!0,__self:!0,__source:!0};function d(a,e,i){var n,s={},_=null,l=null;i!==void 0&&(_=""+i),e.key!==void 0&&(_=""+e.key),e.ref!==void 0&&(l=e.ref);for(n in e)p.call(e,n)&&!k.hasOwnProperty(n)&&(s[n]=e[n]);if(a&&a.defaultProps)for(n in e=a.defaultProps,e)s[n]===void 0&&(s[n]=e[n]);return{$$typeof:f,type:a,key:_,ref:l,props:s,_owner:m.current}}r.Fragment=c,r.jsx=d,r.jsxs=d},2086:function(u,r,t){u.exports=t(1836)}}]);
